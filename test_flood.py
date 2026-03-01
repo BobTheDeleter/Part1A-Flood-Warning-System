751648771ba366a0a773864e21f1a9a9628fd6d3
fiah3 <153201149+fiah3@users.noreply.github.com>
2026-03-01 21:13:37 +0000

    bug fix

--- test_flood.py
+++ test_flood.py
@@ -3,11 +3,11 @@ from floodsystem.flood import stations_level_over_threshold
 
 def test_stations_level_over_threshold():
     s1 = MonitoringStation("test 1", None, None, None, (0, 10), None, None)
-    s2 = MonitoringStation("test 1", None, None, None, (0, 10), None, None)
-    s3 = MonitoringStation("test 2", None, None, None, (0, 10), None, None)
-    s4 = MonitoringStation("test 2", None, None, None, (0, 10), None, None)
-    s5 = MonitoringStation("test 2", None, None, None, (0, 10), None, None)
-    s6 = MonitoringStation("test 2", None, None, None, (-2, 10), None, None)
+    s2 = MonitoringStation("test 2", None, None, None, (0, 10), None, None)
+    s3 = MonitoringStation("test 3", None, None, None, (0, 10), None, None)
+    s4 = MonitoringStation("test 4", None, None, None, (0, 10), None, None)
+    s5 = MonitoringStation("test 5", None, None, None, (0, 10), None, None)
+    s6 = MonitoringStation("test 6", None, None, None, (-2, 10), None, None)
 
     s1.latest_level = 0
     s2.latest_level = 5
@@ -17,8 +17,9 @@ def test_stations_level_over_threshold():
     s6.latest_level = 6
 
     tolerance = 1.0
+    print(stations_level_over_threshold([s1, s2, s3, s4, s5, s6], tolerance))
 
-    assert stations_level_over_threshold([s1, s2, s3, s4, s5, s6], tolerance) == [(s4, 1.25), (s5, 2.0)]
+    assert stations_level_over_threshold([s1, s2, s3, s4, s5, s6], tolerance) == [(s5, 2.0), (s4, 1.25)]
 
 def test_stations_highest_rel_level():
     
